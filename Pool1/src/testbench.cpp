13dc11585854c569167e4792f48dcc38e38c0523
Junnan Shan <30725969+JunnanShan@users.noreply.github.com>
2018-03-04 17:26:07 +0100

    Update testbench.cpp

--- Pool1/src/testbench.cpp
+++ Pool1/src/testbench.cpp
@@ -34,9 +34,8 @@ int main()
 	//initialize the "inp_image" array and print them in order to check it
 
 
-    ifstream inp_file("/home/junnan/Work/Vivado_HLS/pool1/out_image.txt");
+    ifstream inp_file("/home/junnan/Work/Vivado_HLS/Pool1/out_conv1.txt");
     DataType *inp_image;
-//    inp_image = (DataType *)sds_alloc( INP_IMAGE_SIZE * INP_IMAGE_SIZE * INP_IMAGE_CHANNEL * sizeof(DataType));
     inp_image = (DataType *)malloc( INP_IMG_SIZE * INP_IMG_SIZE * INP_IMG_CHAN * sizeof(DataType));
 	if(inp_file.is_open())
 	{
@@ -46,66 +45,23 @@ int main()
 		for (int i=0; i<INP_IMG_SIZE * INP_IMG_SIZE * INP_IMG_CHAN; i++)
 		{
 			inp_file >> inp_image[i];
-			//cout<< "inp_image[" << i << "] = " << inp_image[i] << endl;
 		}
 		inp_file.close();
 	}
-    cout << "inp_image[0] = " << inp_image[0] << endl;
-	/*
-	const float filter[96][3][11][11] = {
-	                           #include "conv1Weights.txt"
-	                         };
-	*/
-/*
-	ifstream filter_file("/home/junnan/Work/Vivado_HLS/conv1_float/conv1Weights.txt");
-	    DataType *filter;
-	   // filter = (DataType *)sds_alloc( FILTER_BATCH*INP_IMAGE_CHANNEL*FILTER_SIZE*FILTER_SIZE * sizeof(DataType));
-	    filter = (DataType *)malloc( FILTER_BATCH*INP_IMAGE_CHANNEL*FILTER_SIZE*FILTER_SIZE * sizeof(DataType));
-	//    inp_image = (float *)malloc( INP_IMAGE_SIZE * INP_IMAGE_SIZE * INP_IMAGE_CHANNEL * sizeof(float));
-		if(filter_file.is_open())
-		{
-			cout << "can open the text file" << endl;
 
+  DataType *out_image = (DataType *)malloc(OUT_IMG_SIZE * OUT_IMG_SIZE * INP_IMG_CHAN * sizeof(DataType));
 
-			for (int i=0; i<FILTER_BATCH*INP_IMAGE_CHANNEL*FILTER_SIZE*FILTER_SIZE; i++)
-			{
-				filter_file >> filter[i];
-				//cout<< "inp_image[" << i << "] = " << inp_image[i];
-			}
-			filter_file.close();
-		}
-	    cout << "filter[0] = " << filter[0] << endl;
-	    cout << "filter[1] = " << filter[1] << endl;
-	    cout << "filter[2] = " << filter[2] << endl;
-	    */
-/*
-  float  inp_image[]  = {
-                         #include "inp_image.txt"
-                      };
-*/
-	//DataType *out_image = (DataType *)sds_alloc(OUT_IMAGE_SIZE * OUT_IMAGE_SIZE * FILTER_BATCH * sizeof(DataType));
-	DataType *out_image = (DataType *)malloc(OUT_IMG_SIZE * OUT_IMG_SIZE * INP_IMG_CHAN * sizeof(DataType));
-//  float *out_image = (float *)malloc(OUT_IMAGE_SIZE * OUT_IMAGE_SIZE * FILTER_BATCH * sizeof(float));
-//  float out_image[OUT_IMAGE_SIZE * OUT_IMAGE_SIZE * FILTER_BATCH];
   cout << "Start calling the conv1 HW function" << endl;
 
-//  std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
   //call the "conv1" function using the "inp_image" argument, it returns the output in the "out_image" array
   pool1(inp_image, out_image);
-
-//  std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
   cout << "After calling the conv1 HW function" << endl;
-//  auto duration = std::chrono::duration_cast<std::chrono::microseconds>(t2-t1).count();
-//  cout << "duration = " << duration << endl;
   //free all the dynamically allocated memory
-//  sds_free(inp_image);
-  //sds_free(inp_image);
-  //sds_free(filter);
-  free(inp_image);
-//  free(filter);
 
+  free(inp_image);
+	
   //dump the output image into a txt file "out_image.txt"
-  ofstream data("/home/junnan/Work/Vivado_HLS/pool1/out_image_pool.txt");
+  ofstream data("/home/junnan/Work/Vivado_HLS/Pool1/out_image_pool.txt");
   for (int k = 0; k < OUT_IMG_SIZE*OUT_IMG_SIZE*INP_IMG_CHAN; k++)
     {
       data << out_image[k] << "\n";
@@ -126,21 +82,11 @@ int main()
 	  }
   }
   cout << "big_diff = " << big_diff << endl;
-  cout << "out_img[69983] = " << out_img[69983] << endl;
-  cout << "out_image[69983] = " << out_image[69983] << endl;
-/*
-  for (int i = 0; i < OUT_IMAGE_SIZE*OUT_IMAGE_SIZE*FILTER_BATCH; i++){
-    if ((out_img[i] - out_image[i] >= 0.001) || (out_img[i] - out_image[i] <= -0.001)){
-    //if (fabs(out_img[i] - out_image[i]) > EPSILON){
-    cout << "out_img[" << i << "] = " <<  out_img[i] << endl;
-    cout << "out_image[" << i << "] = " << out_image[i] << endl;
-    cout << "Functionality failed" << endl;
-         }
-}
-*/
+ 
+
 
   cout << "pooling Functionality pass" << endl;
-  //sds_free(out_image);
+  
   free(out_image);
   return 0;
 }
