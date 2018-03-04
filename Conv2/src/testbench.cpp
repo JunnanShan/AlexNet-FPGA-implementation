b8ceba99e824ef76f7f172bf38d2a3c4e2b5332f
Junnan Shan <30725969+JunnanShan@users.noreply.github.com>
2018-03-04 16:55:10 +0100

    Update testbench.cpp

--- Conv2/src/testbench.cpp
+++ Conv2/src/testbench.cpp
@@ -20,7 +20,6 @@
 //typedef ap_fixed<32,16> DataType;
 typedef float DataType;
 
-//#define EPSILON 0.001
 
 using namespace std;
 
@@ -33,14 +32,10 @@ void conv2(DataType inp_img[INP_IMAGE_SIZE * INP_IMAGE_SIZE * INP_IMAGE_CHANNEL]
 //main function used to test the functionality of the kernel.
 int main()
 {
-  //int input_vector_size_bytes = sizeof(float) * INP_IMAGE_CHANNEL * INP_IMAGE_SIZE * INP_IMAGE_SIZE;
-  //float *inp_image = (float *) malloc(input_vector_size_bytes);
-	//initialize the "inp_image" array and print them in order to check it
-
-	int group =2;
-    ifstream inp_file("/home/junnan/Vivado_HLS/conv2_full/out_padding1.txt");
+    int group =2;
+	
+    ifstream inp_file("/home/junnan/Vivado_HLS/Conv2/out_padding1.txt");
     DataType *inp_image;
-//    inp_image = (DataType *)sds_alloc( INP_IMAGE_SIZE * INP_IMAGE_SIZE * INP_IMAGE_CHANNEL * sizeof(DataType));
     inp_image = (DataType *)malloc( INP_IMAGE_SIZE * INP_IMAGE_SIZE * INP_IMAGE_CHANNEL * sizeof(DataType));
 	if(inp_file.is_open())
 	{
@@ -50,89 +45,54 @@ int main()
 		for (int i=0; i<INP_IMAGE_SIZE * INP_IMAGE_SIZE * INP_IMAGE_CHANNEL; i++)
 		{
 			inp_file >> inp_image[i];
-			//cout<< "inp_image[" << i << "] = " << inp_image[i] << endl;
 		}
 		inp_file.close();
 	}
-    cout << "inp_image[0] = " << inp_image[0] << endl;
-/*
-    DataType filter[256][FILTER_CHANNEL][FILTER_SIZE][FILTER_SIZE] = {
-                                #include "conv2Weights.txt"
-       	  	  	  	  	  	  	  	  	  	  };
-       	  	  	  	  	  	  	  	  	  	  */
 
-
-	ifstream filter_file("/home/junnan/Vivado_HLS/conv2_full/conv2Weights.txt");
+	ifstream filter_file("/home/junnan/Vivado_HLS/Conv2/conv2Weights.txt");
 	    DataType *filter;
 	    filter = (DataType *)malloc( FILTER_BATCH*FILTER_CHANNEL*FILTER_SIZE*FILTER_SIZE * sizeof(DataType));
 		if(filter_file.is_open())
 		{
 			cout << "can open the text file" << endl;
-
-
 			for (int i=0; i<FILTER_BATCH*INP_IMAGE_CHANNEL*FILTER_SIZE*FILTER_SIZE; i++)
 			{
 				filter_file >> filter[i];
-				//cout<< "inp_image[" << i << "] = " << inp_image[i];
 			}
 			filter_file.close();
 		}
-	    cout << "filter[0] = " << filter[0] << endl;
-	    cout << "filter[1] = " << filter[1] << endl;
-	    cout << "filter[2] = " << filter[2] << endl;
 
-		ifstream bias_file("/home/junnan/Vivado_HLS/conv2_full/conv2Bias.txt");
+		ifstream bias_file("/home/junnan/Vivado_HLS/Conv2/conv2Bias.txt");
 		    DataType *bias;
 		    bias = (DataType *)malloc( FILTER_BATCH * sizeof(DataType));
 			if(bias_file.is_open())
 			{
 				cout << "can open the text file" << endl;
-
-
 				for (int i=0; i<FILTER_BATCH; i++)
 				{
 					bias_file >> bias[i];
-					//cout<< "inp_image[" << i << "] = " << inp_image[i];
 				}
 				bias_file.close();
 			}
-		    cout << "bias[0] = " << bias[0] << endl;
-		    cout << "bias[1] = " << bias[1] << endl;
-		    cout << "bias[2] = " << bias[2] << endl;
 
-/*
-  float  inp_image[]  = {
-                         #include "inp_image.txt"
-                      };
-*/
-	//DataType *out_image = (DataType *)sds_alloc(OUT_IMAGE_SIZE * OUT_IMAGE_SIZE * FILTER_BATCH * sizeof(DataType));
 	DataType *out_image = (DataType *)malloc(OUT_IMAGE_SIZE * OUT_IMAGE_SIZE * FILTER_BATCH * sizeof(DataType));
-//  float *out_image = (float *)malloc(OUT_IMAGE_SIZE * OUT_IMAGE_SIZE * FILTER_BATCH * sizeof(float));
-//  float out_image[OUT_IMAGE_SIZE * OUT_IMAGE_SIZE * FILTER_BATCH];
-  cout << "Start calling the conv1 HW function" << endl;
+ 
+	cout << "Start calling the conv1 HW function" << endl;
 
-//  std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
   //call the "conv1" function using the "inp_image" argument, it returns the output in the "out_image" array
   conv2(inp_image, out_image, filter, bias);
 
-//  std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
   cout << "After calling the conv1 HW function" << endl;
-//  auto duration = std::chrono::duration_cast<std::chrono::microseconds>(t2-t1).count();
-//  cout << "duration = " << duration << endl;
-  //free all the dynamically allocated memory
-//  sds_free(inp_image);
-  //sds_free(inp_image);
-  //sds_free(filter);
+	
   free(inp_image);
-  //free(filter);
+  free(filter);
+  free(bias);
 
   //dump the output image into a txt file "out_image.txt"
-  ofstream data("/home/junnan/Vivado_HLS/conv2_full/out_convolution2.txt");
+  ofstream data("/home/junnan/Vivado_HLS/Conv2/out_convolution2.txt");
   for (int k = 0; k < OUT_IMAGE_SIZE*OUT_IMAGE_SIZE*FILTER_BATCH; k++)
     {
       data << out_image[k] << "\n";
-      //if(out_image[k] != 0)
-      //cout << "out_image[" << k << "] = " << out_image[k] << endl;
     }
 
 
@@ -150,23 +110,7 @@ int main()
 		 big_diff = diff[i];
 	  }
   }
-  cout << "big_diff = " << big_diff << endl;
-  cout << "out_img[11] = " << out_img[11] << endl;
-  cout << "out_image[11] = " << out_image[11] << endl;
 
-/*
-  for (int i = 0; i < OUT_IMAGE_SIZE*OUT_IMAGE_SIZE*FILTER_BATCH; i++){
-    if ((out_img[i] - out_image[i] >= 0.001) || (out_img[i] - out_image[i] <= -0.001)){
-    //if (fabs(out_img[i] - out_image[i]) > EPSILON){
-    cout << "out_img[" << i << "] = " <<  out_img[i] << endl;
-    cout << "out_image[" << i << "] = " << out_image[i] << endl;
-    cout << "Functionality failed" << endl;
-         }
-}
-*/
-
-//  cout << "Functionality pass" << endl;
-  //sds_free(out_image);
   free(out_image);
   return 0;
 }
