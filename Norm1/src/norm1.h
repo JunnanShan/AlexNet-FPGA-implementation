517a9880fcb62902541dbda1092be37ac039ec61
Junnan Shan <30725969+JunnanShan@users.noreply.github.com>
2018-03-05 15:34:25 +0100

    Update norm1.h

--- Norm1/src/norm1.h
+++ Norm1/src/norm1.h
@@ -1,3 +1,10 @@
+/* 
+======================================================
+*
+* Author:   Junnan Shan (junnan.shan@polito.it)
+*
+======================================================
+*/
 #define INP_IMAGE_SIZE 27
 #define INP_IMAGE_CHANNEL 96
 #define OUT_IMAGE_SIZE 27
