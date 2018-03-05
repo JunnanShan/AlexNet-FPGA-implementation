cf8c29b186d3a3f2f976f1fbbceb75be8c336b2c
Junnan Shan <30725969+JunnanShan@users.noreply.github.com>
2018-03-05 15:55:26 +0100

    Update pad4.h

--- Pad4/src/pad4.h
+++ Pad4/src/pad4.h
@@ -1,4 +1,10 @@
-
+/* 
+======================================================
+*
+* Author:   Junnan Shan (junnan.shan@polito.it)
+*
+======================================================
+*/
 #define INP_IMAGE_SIZE 13
 #define INP_IMAGE_CHANNEL 384
 #define OUT_IMAGE_SIZE 15
