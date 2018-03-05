f82258ef5cbb57f92a2d2fad8bb9d6842d364a36
Junnan Shan <30725969+JunnanShan@users.noreply.github.com>
2018-03-05 13:54:59 +0100

    Add files via upload

---
+++ Conv3/src/conv3.h
@@ -0,0 +1,18 @@
+/*
+#define INP_IMAGE_SIZE 15
+#define INP_IMAGE_CHANNEL 3
+#define OUT_IMAGE_SIZE 2
+#define FILTER_BATCH 3
+#define FILTER_SIZE 11
+#define STRIDE 4
+#define STRIDE_NEW 11
+*/
+#define INP_IMAGE_SIZE 15
+#define INP_IMAGE_CHANNEL 256
+#define OUT_IMAGE_SIZE 13
+#define FILTER_BATCH 384
+//#define FILTER_CHANNEL 256
+#define FILTER_SIZE 3
+#define STRIDE 1
+#define STRIDE_NEW 3
+
