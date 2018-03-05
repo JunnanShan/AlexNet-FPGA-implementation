14080adef3271c3c59385b98816f07ce91506895
Junnan Shan <30725969+JunnanShan@users.noreply.github.com>
2018-03-05 16:00:20 +0100

    Update pool5.h

--- Pool5/src/pool5.h
+++ Pool5/src/pool5.h
@@ -1,3 +1,10 @@
+/* 
+======================================================
+*
+* Author:   Junnan Shan (junnan.shan@polito.it)
+*
+======================================================
+*/
 #define INP_IMG_CHAN 256
 #define INP_IMG_SIZE 13
 #define OUT_IMG_SIZE 6
