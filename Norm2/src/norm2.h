2f77b9331ac6c7b3fef4af982cedb9cdc6bd2cd2
Junnan Shan <30725969+JunnanShan@users.noreply.github.com>
2018-03-05 15:38:29 +0100

    Update norm2.h

--- Norm2/src/norm2.h
+++ Norm2/src/norm2.h
@@ -1,3 +1,10 @@
+/* 
+======================================================
+*
+* Author:   Junnan Shan (junnan.shan@polito.it)
+*
+======================================================
+*/
 #define INP_IMAGE_SIZE 13
 #define INP_IMAGE_CHANNEL 256
 #define OUT_IMAGE_SIZE 13
