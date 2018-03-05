ae0f10827522fa03bfeed9bda6f58015d834a7e8
Junnan Shan <30725969+JunnanShan@users.noreply.github.com>
2018-03-05 14:36:43 +0100

    Add files via upload

---
+++ Pad3/src/testbench.cpp
@@ -0,0 +1,83 @@
+//in this c++ file, we define the 1st convolution layer of Alexnet. Actually it's not just convolution, it's convolution together with
+//Relu, we just compare the output of the convolution, if it's greater than 0, then we store it as output, if it's negative, we store it as 0.
+//Also in caffe the Relu layer is inlined with the other layer.
+#include <sys/types.h>
+#include <sys/stat.h>
+//#include <fcnt1.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <cstring>
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+#include <fstream>
+//#include <chrono>
+//#include "sds_lib.h"
+#include "padding3.h"
+#include "ap_fixed.h"
+
+//typedef ap_fixed<32,16> DataType;
+typedef float DataType;
+
+
+//#define EPSILON 0.001
+
+using namespace std;
+
+void padding3(DataType inp_img[INP_IMAGE_SIZE * INP_IMAGE_SIZE * INP_IMAGE_CHANNEL],
+	     DataType out_img[OUT_IMAGE_SIZE * OUT_IMAGE_SIZE * OUT_IMAGE_CHANNEL]);
+
+
+//main function used to test the functionality of the kernel.
+int main()
+{
+
+	//int pad = 2;
+
+
+    ifstream inp_file("/home/junnan/Vivado_HLS/padding3/out_conv3.txt");
+    DataType *inp_image;
+//    inp_image = (DataType *)sds_alloc( INP_IMAGE_SIZE * INP_IMAGE_SIZE * INP_IMAGE_CHANNEL * sizeof(DataType));
+    inp_image = (DataType *)malloc( INP_IMAGE_SIZE * INP_IMAGE_SIZE * INP_IMAGE_CHANNEL * sizeof(DataType));
+	if(inp_file.is_open())
+	{
+		cout << "can open the text file" << endl;
+
+
+		for (int i=0; i<INP_IMAGE_SIZE * INP_IMAGE_SIZE * INP_IMAGE_CHANNEL; i++)
+		{
+			inp_file >> inp_image[i];
+			//cout<< "inp_image[" << i << "] = " << inp_image[i] << endl;
+		}
+		inp_file.close();
+	}
+
+
+	DataType *out_image = (DataType *)malloc(OUT_IMAGE_SIZE * OUT_IMAGE_SIZE * OUT_IMAGE_CHANNEL * sizeof(DataType));
+
+  cout << "Start calling the conv1 HW function" << endl;
+
+  //call the "conv1" function using the "inp_image" argument, it returns the output in the "out_image" array
+  padding3(inp_image, out_image);
+
+//  std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
+  cout << "After calling the conv1 HW function" << endl;
+
+  free(inp_image);
+
+
+  //dump the output image into a txt file "out_image.txt"
+  ofstream data("/home/junnan/Vivado_HLS/padding3/inp_conv4.txt");
+  for (int k = 0; k < OUT_IMAGE_SIZE*OUT_IMAGE_SIZE*OUT_IMAGE_CHANNEL; k++)
+    {
+      data << out_image[k] << "\n";
+      //cout << "out_image[" << k << "] = " << out_image[k] << endl;
+    }
+
+
+//  cout << "Functionality pass" << endl;
+  //sds_free(out_image);
+  free(out_image);
+  return 0;
+}
