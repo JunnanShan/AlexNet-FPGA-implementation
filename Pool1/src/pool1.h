08ca5f50a4c10492c936e610cfb98d7f3ae7c1db
Junnan Shan <30725969+JunnanShan@users.noreply.github.com>
2018-03-05 15:56:59 +0100

    Update pool1.h

--- Pool1/src/pool1.h
+++ Pool1/src/pool1.h
@@ -1,3 +1,10 @@
+/* 
+======================================================
+*
+* Author:   Junnan Shan (junnan.shan@polito.it)
+*
+======================================================
+*/
 #define INP_IMG_CHAN 96
 #define INP_IMG_SIZE 55
 #define OUT_IMG_SIZE 27
